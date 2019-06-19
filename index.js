b13d825ec20fccdedbef3c32144cedcd92321aeb
Sergey <slunev@eccentex.com>
2019-06-19 16:58:52 +0300

    fix mistakes after review

--- index.js
+++ index.js
@@ -1,83 +1,84 @@
 function FormParamsObject(params) {
     this.params = params;
-}
 
-function getFullObjectImpl() {
-    return JSON.stringify(this.params, null, 4);
-}
-
-function getObjectPropertyImpl(obj) {
-    let result = this.params;
-    let propArray = obj.split('.');
-
-    if (propArray.length >= 0) {
-        for (var i = 0; i < propArray.length; i++) {
-            if (result === undefined) break;
-            result = result[propArray[i]];
-        }
+    this.getFullObject = function() {
+        return JSON.stringify(this.params, null, 4);
     }
-
-    return result === undefined ? null : result;
-}
-
-function setObjectPropertyImpl(propPath, value) {
-    let result = this.params;
-    let propArray = propPath.split('.');
-    let propLen = propArray.length;
-
-    for (let i = 0; i < propLen - 1; i++) {
-        let elem = propArray[i];
-        if (!result[elem]) {
-            result[elem] = {};
-        } else {
-            if (typeof(result[elem]) == "string") {
-                result[elem] = {};
+    
+    this.getObjectProperty = function(obj) {
+        var result = this.params;
+        var propArray = obj.split('.');
+    
+        if (propArray.length >= 0) {
+            for (var i = 0; i < propArray.length; i++) {
+                if (result === undefined) break;
+                result = result[propArray[i]];
             }
         }
-        result = result[elem];
+    
+        return result === undefined ? null : result;
     }
-    result[propArray[propLen - 1]] = value;
 
-    return result[propArray[propLen - 1]];
-}
-
-function convertObjectToArrayImpl(obj) {
-    let mainObj = params.getObjectProperty(obj);
-
-    let newObj = {};
-    let result = [];
-    let keyObj = Object.keys(mainObj);
-    let size = keyObj.length;
-
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < mainObj[keyObj[i]].length; j++) {
-            newObj[keyObj[j]] = mainObj[keyObj[j]][i];
+    this.setObjectProperty = function(propPath, value) {
+        var result = this.params;
+        var propArray = propPath.split('.');
+        var propLen = propArray.length;
+    
+        for (var i = 0; i < propLen - 1; i++) {
+            var elem = propArray[i];
+            if (!result[elem]) {
+                result[elem] = {};
+            } else {
+                if (typeof(result[elem]) != "object") {
+                    result[elem] = {};
+                }
+            }
+            result = result[elem];
         }
-        result.push(newObj);
-        newObj = {};
+        result[propArray[propLen - 1]] = value;
+    
+        return result[propArray[propLen - 1]];
     }
-    params.setObjectProperty(obj, result);
 
-    return result === undefined ? null : result;
+    this.convertObjectToArray = function(obj) {   
+        var mainObj = this.getObjectProperty(obj);  
+        var newObj = {};
+        var result = [];
+        var keyObj = Object.keys(mainObj);
+        var size = Object.keys(mainObj).length;
+        var maxLength = 0;
+    
+        for (var i = 0; i < size; i++) {
+            if (maxLength < mainObj[keyObj[i]].length) maxLength = mainObj[keyObj[i]].length;
+        }
+    
+        for (var i = 0; i < maxLength; i++) {
+            for (var j = 0; j < maxLength; j++) {    
+                if (!keyObj[j] || !mainObj[keyObj[j]][i]) continue;
+                newObj[keyObj[j]] = mainObj[keyObj[j]][i];
+            }
+            result.push(newObj);
+            newObj = {};
+        }
+        
+        this.setObjectProperty(obj, result);
+        return result;
+    }
 }
+
 
 var params = new FormParamsObject({
     param1: 'test1',
     param2: {
         param21: 'test2',
         param22: {
-            number: ["123", "456"],
-            text: ["text1", "text2"],
+            number: ["123", "456", "22"],
+            text: ["text1", "text2"]
         }
     }
 });
 
-params.getFullObject = getFullObjectImpl;
-params.getObjectProperty = getObjectPropertyImpl;
-params.setObjectProperty = setObjectPropertyImpl;
-params.convertObjectToArray = convertObjectToArrayImpl;
-
-
+// params.getFullObject();
 // params.getObjectProperty('param2.param21');
 // params.getObjectProperty('param1.param11.param33'); 
 // params.setObjectProperty('param2.param21', 'new value');
@@ -85,3 +86,6 @@ params.convertObjectToArray = convertObjectToArrayImpl;
 // params.getObjectProperty('param1.param11.param33'); 
 // params.convertObjectToArray('param2.param22');
 // params.getFullObject();
+
+// params.setObjectProperty('param2.param21', 1);
+// params.setObjectProperty('param2.param21.p2', 2);
